c5659be3a12fdfed51808ae145c10c7041023086
tostos <saittosun2@gmail.com>
2020-09-23 20:37:51 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'sait-tosun-assignment';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
